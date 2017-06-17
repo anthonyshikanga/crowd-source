96eabd2c9ab38333538f1de7860389e4fd3fff2c
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-18 02:57:03 +0300

    modified index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      festivals: this.store.findAll('festival')
+    })
+  },
+  actions: {
+    saveNewFestival(params){
+      var newFestival= this.store.createRecord('festival',params);
+      newFestival.save(),
+      this.transitionTo('index')
+    }
+  }
 });
