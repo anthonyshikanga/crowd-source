c0aa3fb708cac4cf8c36b8c71e7770f8d17e13a0
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-18 04:06:28 +0300

    added new component

---
+++ app/components/new-festival.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewFestival: false,
+  actions: {
+    festivalFormShow(){
+      this.set('addNewFestival',true)
+    },
+    cancel(){
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('content',''),
+      this.set('openDate',''),
+      this.set('artist',''),
+      this.set('artistImage',''),
+      this.set('location',''),
+      this.set('venueImage',''),
+      this.set('addNewFestival',false)
+    },
+    reset(){
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('content',''),
+      this.set('openDate',''),
+      this.set('artist',''),
+      this.set('artistImage',''),
+      this.set('location',''),
+      this.set('venueImage',''),
+    },
+    saveNewFestival(){
+      var params= {
+        name:this.get('name',''),
+        category:this.get('category',''),
+        shortDescription:this.get('shortDescription',''),
+        content:this.get('content',''),
+        openDate:this.get('openDate',''),
+        artist:this.get('artist',''),
+        artistImage:this.get('artistImage',''),
+        location:this.get('location',''),
+        venueImage:this.get('venueImage',''),
+      };
+      this.set('name',''),
+      this.set('category',''),
+      this.set('shortDescription',''),
+      this.set('description',''),
+      this.set('dateOpened',''),
+      this.set('website',''),
+      this.set('phone',''),
+      this.set('locationName',''),
+      this.set('locationLongitude',''),
+      this.set('locationLongitude',''),
+      this.set('addNewRestaurant',false),
+      this.set('image'),
+this.sendAction('saveNewRestaurant',params)
+    }
+  }
+});
