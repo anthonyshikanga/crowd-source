e7d65318be4580f09ad75bcd122c05591bf71706
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-21 12:41:09 +0300

    Added about template

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,12 +5,5 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       festivals: this.store.findAll('festival')
     })
-  },
-  actions: {
-    saveNewFestival(params){
-      var newFestival= this.store.createRecord('festival',params);
-      newFestival.save(),
-      this.transitionTo('index')
-    }
   }
 });
